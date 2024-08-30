971fe43d725b6beb7524d0d0a1b755dac47add3c
miyasajid19 <miyasajid19@gmail.com>
2024-08-30 14:35:41 +0530

    reversing string using stacks

--- week3/Question_2_arr.cpp
+++ week3/Question_2_arr.cpp
@@ -1,121 +1,41 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-using namespace std;
-class Stacks
-{
-    int size;
-    int capacity;
-    char *arr;
+#include "stacks.h"
 
-public:
-    Stacks() {}
-    Stacks(int capacity)
-    {
-        this->size = 0;
-        this->capacity = capacity;
-        arr = new char[capacity + 1];
-    }
-    void setStack(int capacity)
-    {
-        this->size = 0;
-        this->capacity = capacity;
-        arr = new char[capacity + 1];
-    }
-    void reStack(int capacity)
-    {
-        this->capacity = capacity;
-        char *arr1 = new char[capacity + 1];
-        for (int i = 0; i < size; i++)
-        {
-            arr1[i] = arr[i];
-        }
-        delete[] arr;
-        arr = arr1;
-    }
-    bool isEmpty()
-    {
-        return this->size == 0;
-    }
-    bool isFull()
-    {
-        return this->size == this->capacity;
-    }
-    int Size()
-    {
-        return size;
-    }
-    void Push(char value)
-    {
-        if (isFull())
-        {
-            cout << "stack overflow" << endl;
-            return;
-        }
-        arr[this->size++] = value;
-    }
-    void Pop()
-    {
-        if (isEmpty())
-        {
-            cout << "stack underflow" << endl;
-            return;
-        }
-        this->size--;
-    }
-    char Peek()
-    {
-        if (isEmpty())
-        {
-            cout << "stack is empty" << endl;
-            return -1;
-        }
-        return arr[this->size - 1];
-    }
-    void display()
-    {
-        if (isEmpty())
-        {
-            cout << "stack is empty" << endl;
-            return;
-        }
-        for (int i = 0; i < size; i++)
-        {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-    }
-};
-string reverse(string str)
-{
-    Stacks temp(str.size());
-    for (int i = 0; i < str.size(); i++)
-    {
+using namespace std;
+
+template <typename T>
+T reverse(T str) {
+    Stacks<char> temp(str.size());
+    for (int i = 0; i < str.size(); i++) {
         cout << str[i] << " ";
         temp.Push(str[i]);
     }
     cout << endl;
-    string result;
-    while (not temp.isEmpty())
-    {
+
+    T result;
+    while (!temp.isEmpty()) {
         result += temp.Peek();
         temp.Pop();
     }
+
     return result;
 }
-int main()
-{
+
+int main() {
     // #ifndef ONLINE_JUDGE
     // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
     // #endif
 
-    Stacks stack;
-    int capacity, choice, value;
     string str;
-    cout << "Enter the string";
+    cout << "Enter the string: ";
     getline(cin, str);
+
     string reversed = reverse(str);
-    cout << "the reversed string is " << reversed;
+    cout << "The reversed string is: " << reversed << endl;
+
+
     return EXIT_SUCCESS;
 }
