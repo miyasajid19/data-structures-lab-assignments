28ad54c5eded3fc2207eb311a43ae04f0709c021
miyasajid19 <miyasajid19@gmail.com>
2024-08-30 19:34:54 +0530

    evaluate numeric postfix operations

---
+++ week3/Question4_evaluate_Postfix.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "stacks.h" 
+using namespace std;
+
+string evaluatePostfix(const string &str)
+{
+    Stacks<string> stack(str.length());
+    string result;
+
+    for (char a : str)
+    {
+        result = "";
+        string x(1, a);
+
+        if (x == "^" || x == "*" || x == "%" || x == "-" || x == "/" || x == "+")
+        {
+            string second = stack.Peek();
+            stack.Pop();
+            string first = stack.Peek();
+            stack.Pop();
+            result = '(' + first + ' ' + x + ' ' + second + ')';
+            stack.Push(result);
+            result = stack.Peek();
+        }
+        else
+        {
+            stack.Push(x);
+        }
+    }
+
+    return stack.Peek();
+}
+int main()
+{
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    cout << "Enter a postfix expression to convert it to infix: ";
+    string str;
+    cin >> str;
+
+    string infix = evaluatePostfix(str);
+
+    cout << "The corresponding infix expression is: " << infix << endl;
+
+    return EXIT_SUCCESS;
+}
