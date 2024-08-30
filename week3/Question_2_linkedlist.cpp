14f09e7488e5797e860c1332be32afb1d975775c
miyasajid19 <miyasajid19@gmail.com>
2024-08-30 15:04:32 +0530

    reverse the string using stacks

---
+++ week3/Question_2_linkedlist.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "Stacks_linkedlist.h"
+using namespace std;
+template <typename T>
+T reverse(T str)
+{
+    Stacks<char> temp(str.size());
+    for (int i = 0; i < str.size(); i++)
+    {
+        temp.Push(str[i]);
+    }
+    T result;
+    while (!temp.isEmpty())
+    {
+        result += temp.Peek();
+        temp.Pop();
+    }
+
+    return result;
+}
+int main()
+{
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+
+    string str;
+    cout << "Enter the string: ";
+    getline(cin, str);
+
+    string reversed = reverse(str);
+    cout << "The reversed string is: " << reversed << endl;
+
+    return EXIT_SUCCESS;
+}
