5782bfd81ae1721e98a455e1a786497bb8c9e0b4
miyasajid19 <miyasajid19@gmail.com>
2024-09-08 12:29:16 +0530

    add solution for third question

--- week4/Question_3.cpp
+++ week4/Question_3.cpp
@@ -2,48 +2,56 @@
 #include <cstdlib>
 #include "CircularQueue.h"
 using namespace std;
-void Interleave(Queue<int> &queue)
-{
-    int x=queue.Size();
-    int size = queue.Size() / 2;
-    Queue<int> temp(size);
-    for (int i = 0; i < size; i++)
-    {
+
+void Interleave(Queue<int> &queue) {
+    int totalSize = queue.Size();
+    int halfSize = totalSize / 2;
+    Queue<int> temp(halfSize);
+
+    // Transfer the first half of the queue to the temporary queue
+    for (int i = 0; i < halfSize; i++) {
         temp.Enqueue(queue.Peek());
         queue.Dequeue();
     }
-    for(int i =0;i<x;i++)
-    {
-        if(i%2==0)
-        {
+
+    // Interleave elements from the temporary queue and the original queue
+    for (int i = 0; i < totalSize; i++) {
+        if (i % 2 == 0) {
             queue.Enqueue(temp.Peek());
             temp.Dequeue();
-        }
-        else{
+        } else {
             queue.Enqueue(queue.Peek());
             queue.Dequeue();
         }
     }
 }
-int main()
-{
-    // #ifndef ONLINE_JUDGE
-    //     freopen("input.txt", "r", stdin);
-    //     freopen("output.txt", "w", stdout);
-    // #endif
+
+int main() {
     int size;
-    cout << "enter how many data do you want to insert in the queue ?" << endl;
+    cout << "Enter how many data do you want to insert in the queue? " << endl;
     cin >> size;
-    Queue<int> queue(size+1);
-    for (int i = 0; i < size; i++)
-    {
+
+    // Initialize queue with size + 1 for demonstration
+    Queue<int> queue(size + 1);
+
+    // Enqueue elements
+    for (int i = 0; i < size; i++) {
         int num;
-        cout << "enter element :: ";
+        cout << "Enter element: ";
         cin >> num;
         queue.Enqueue(num);
     }
+
+    // Display the original queue
+    cout << "Original Queue: ";
     queue.Display();
+
+    // Perform interleaving
     Interleave(queue);
+
+    // Display the interleaved queue
+    cout << "Interleaved Queue: ";
     queue.Display();
+
     return EXIT_SUCCESS;
 }
