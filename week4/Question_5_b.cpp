84b22988e8e8cff20de65852069e396b5b654fbe
miyasajid19 <miyasajid19@gmail.com>
2024-09-08 17:25:31 +0530

    add solution of second part for fifth question

--- week4/Question_5_b.cpp
+++ week4/Question_5_b.cpp
@@ -2,59 +2,81 @@
 #include <cstdlib>
 #include "linearQueue.h"
 using namespace std;
+
 class Stacks : public Queue<int>
 {
 public:
     Stacks(int capacity) : Queue(capacity) {}
+
     void Push(int value)
     {
         if (isFull())
         {
-            cout << "stack overflow" << endl;
+            cout << "Stack Overflow" << endl;
             return;
         }
         Enqueue(value);
-        while (Size() > 1)
+        int size = Size();
+        for (int i = 0; i < size - 1; ++i)
         {
             int x = Peek();
             Dequeue();
             Enqueue(x);
         }
     }
+
     void Pop()
     {
-        if(isEmpty())
+        if (isEmpty())
         {
-            cout<<"stack underflow"<<endl;
+            cout << "Stack Underflow" << endl;
             return;
         }
         Dequeue();
     }
+
     int Top()
     {
-        if(isEmpty())
+        if (isEmpty())
         {
-            cout<<"stack is empty"<<endl;
+            cout << "Stack is empty" << endl;
             return -1;
         }
         return Peek();
     }
 };
+
 int main()
 {
     // #ifndef ONLINE_JUDGE
     //     freopen("input.txt", "r", stdin);
     //     freopen("output.txt", "w", stdout);
     // #endif
-Stacks stack(3);
-stack.Push(1);
-stack.Display();
-cout<<"Top:::: "<<stack.Top()<<endl;
-stack.Push(2);
-stack.Display();
-cout<<"Top:::: "<<stack.Top()<<endl;
-stack.Push(3);
-stack.Display();
-cout<<"Top:::: "<<stack.Top()<<endl;
+
+    Stacks stack(3);
+    stack.Push(1);
+    stack.Display();
+    cout << "Top: " << stack.Top() << endl;
+
+    stack.Push(2);
+    stack.Display();
+    cout << "Top: " << stack.Top() << endl;
+
+    stack.Push(3);
+    stack.Display();
+    cout << "Top: " << stack.Top() << endl;
+
+    stack.Pop();
+    stack.Display();
+    cout << "Top: " << stack.Top() << endl;
+
+    stack.Pop();
+    stack.Display();
+    cout << "Top: " << stack.Top() << endl;
+
+    stack.Pop();
+    stack.Display();
+    cout << "Top: " << stack.Top() << endl;
+
     return EXIT_SUCCESS;
 }
