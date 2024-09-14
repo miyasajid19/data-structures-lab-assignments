b3307882547b0edd9202aa2f32676fc27bc389ef
miyasajid19 <miyasajid19@gmail.com>
2024-09-14 16:20:58 +0530

    add solution for the third  quesions

--- week5/Question_2.cpp
+++ week5/Question_2.cpp
@@ -22,23 +22,36 @@ linkedList removeKey(Node *&head, int key, int &count)
 }
 int main()
 {
-    // #ifndef ONLINE_JUDGE
-    //     freopen("input.txt", "r", stdin);
-    //     freopen("output.txt", "w", stdout);
-    // #endif
     linkedList ll;
-    ll.InsertAtTail(1);
-    ll.InsertAtTail(2);
-    ll.InsertAtTail(1);
-    ll.InsertAtTail(2);
-    ll.InsertAtTail(1);
-    ll.InsertAtTail(3);
-    ll.InsertAtTail(4);
+
+    int numElements;
+    cout << "Enter the number of elements for the linked list: ";
+    cin >> numElements;
+
+    cout << "Enter the elements for the linked list:" << endl;
+    for (int i = 0; i < numElements; i++)
+    {
+        int value;
+        cout << "Element " << i + 1 << ": ";
+        cin >> value;
+        ll.InsertAtTail(value);
+    }
+
+    cout << "The original linked list is: ";
     ll.display();
+
+    int key;
+    cout << "Enter the key value to remove from the linked list: ";
+    cin >> key;
+
     int count = 0;
-    linkedList ans = removeKey(ll.head, 1, count);
-    cout << "Results :: ";
-    cout << "the total no. of key nodes are " << count << endl;
+    linkedList ans = removeKey(ll.head, key, count);
+
+    cout << "Results: " << endl;
+    cout << "The total number of nodes with the key value " << key << " removed: " << count << endl;
+    
+    cout << "The updated linked list is: ";
     ans.display();
+
     return EXIT_SUCCESS;
 }
