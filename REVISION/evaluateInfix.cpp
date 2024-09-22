1114389b6e774cdc0bb00008c299c833890c629e
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-23 01:55:46 +0530

    create template

---
+++ REVISION/evaluateInfix.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    return EXIT_SUCCESS;
+}
