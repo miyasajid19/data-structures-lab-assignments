bf01dcc7a12920fb3fa4b2a3e85fbb8a7c2aef61
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-23 01:47:38 +0530

    covert prefix to equivalent infix expression

---
+++ REVISION/prefixToinfix.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+#include <stack>
+#include <string>
+#include <algorithm>
+using namespace std;
+string postfixToInfix(string str)
+{
+    reverse(str.begin(),str.end());
+    stack<string> stk;
+    string result;
+    for (char x : str)
+    {
+        if ((x >= 'a' and x <= 'z') or (x >= 'A' and x <= 'Z') or (x >= '0' and x <= '9'))
+        {
+            stk.push(string(1, x));
+        }
+        else
+        {
+            string second = stk.top();
+            stk.pop();
+            string first = stk.top();
+            stk.pop();
+            result = "(" + second + " " + x + " " + first + ")";
+            stk.push(result);
+        }
+    }
+    return stk.top();
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << "Enter the infix exppression : " << endl;
+    string str;
+    getline(cin, str);
+    cout << "the equivalennt postfix operation is  :: " << postfixToInfix(str);
+    return EXIT_SUCCESS;
+}
