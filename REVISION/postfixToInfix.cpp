c98e6ffe4e70defd2aa76358c1789c4a1998efc5
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-23 00:51:52 +0530

    covert postfix expression to the equivalent infix operation

--- REVISION/postfixToInfix.cpp
+++ REVISION/postfixToInfix.cpp
@@ -3,6 +3,47 @@
 #include <stack>
 #include <string>
 using namespace std;
+int precedence(char x)
+{
+    if (x == '^')
+    {
+        return 3;
+    }
+    else if (x == '*' or x == '/' or x == '%')
+    {
+        return 2;
+    }
+    else if (x == '+' or x == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+string postfixToInfix(string str)
+{
+    stack<string> stk;
+    string result;
+    for (char x : str)
+    {
+        if ((x >= 'a' and x <= 'z') or (x >= 'A' and x <= 'Z') or (x >= '0' and x <= '9'))
+        {
+            stk.push(string(1, x));
+        }
+        else
+        {
+            string second = stk.top();
+            stk.pop();
+            string first = stk.top();
+            stk.pop();
+            result = "(" + first + " " + x + " " + second + ")";
+            stk.push(result);
+        }
+    }
+    return stk.top();
+}
 
 int main()
 {
@@ -10,6 +51,9 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-
+    cout << "Enter the infix exppression : " << endl;
+    string str;
+    getline(cin, str);
+    cout << "the equivalennt postfix operation is  :: " << postfixToInfix(str);
     return EXIT_SUCCESS;
 }
