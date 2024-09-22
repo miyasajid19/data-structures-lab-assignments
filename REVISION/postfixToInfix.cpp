7aa00b5a9354c00b775f510b517159ce8b1959ca
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-23 00:38:44 +0530

    first commt

---
+++ REVISION/postfixToInfix.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    return EXIT_SUCCESS;
+}
