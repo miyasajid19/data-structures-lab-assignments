ecf92eb47abf22e6851a0cae4f904b8e71ac19e4
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-23 10:27:35 +0530

    reversing of the stacks using recurrsioin and without using second stacks

--- REVISION/reverseStack_Recurrsion.cpp
+++ REVISION/reverseStack_Recurrsion.cpp
@@ -32,7 +32,6 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    // Initialize a stack and push some elements
     stack<int> stk;
     cout << "Pushing 1 onto the stack.\n";
     stk.push(1);
@@ -43,14 +42,11 @@ int main()
     cout << "Pushing 3 onto the stack.\n";
     stk.push(3);
 
-    // Display the top element of the stack
     cout << "Current top element of the stack: " << stk.top() << endl;
 
-    // Reverse the stack
     cout << "Reversing the stack...\n";
     Reverse(stk);
 
-    // Display the new top element after reversing the stack
     cout << "New top element after reversing the stack: " << stk.top() << endl;
 
     return EXIT_SUCCESS;
