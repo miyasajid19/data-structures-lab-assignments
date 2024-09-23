6637c2f773a3d13d207b8c0763ce08f79060fced
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-23 16:24:45 +0530

    reversal of Queue using stacks

---
+++ REVISION/ReverseQueue.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+using namespace std;
+void Reverse(queue<int> &Q)
+{
+    stack<int> temp;
+    while (!Q.empty())
+    {
+        temp.push(Q.front());
+        Q.pop();
+    }
+    while (!temp.empty())
+    {
+        Q.push(temp.top());
+        temp.pop();
+    }
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    queue<int> Q;
+    for (int i = 0; i < 5; i++)
+        Q.push(i);
+    queue<int> temp = Q;
+    cout << "original Queue ::: ";
+    while (!temp.empty())
+    {
+        cout << temp.front() << " ";
+        temp.pop();
+    }
+    cout << endl;
+    cout << "ReversedF Queue ::: ";
+    Reverse(Q);
+    temp = Q;
+    while (!temp.empty())
+    {
+        cout << temp.front() << " ";
+        temp.pop();
+    }
+    cout << endl;
+    return EXIT_SUCCESS;
+}
