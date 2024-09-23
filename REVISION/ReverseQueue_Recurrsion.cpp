67ea09203df1b45a43d242a15c916fbaab8d6498
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-23 16:32:09 +0530

    reversal of Queue using recurrsion

---
+++ REVISION/ReverseQueue_Recurrsion.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+using namespace std;
+void Reverse(queue<int> &Q)
+{
+    if(Q.empty())
+    {
+        return;
+    }
+    int x=Q.front();
+    Q.pop();
+    Reverse(Q);
+    Q.push(x);
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    queue<int> Q;
+    for (int i = 0; i <5; i++)
+        Q.push(i);
+    queue<int> temp = Q;
+    cout << "original Queue ::: ";
+    while (!temp.empty())
+    {
+        cout << temp.front() << " ";
+        temp.pop();
+    }
+    cout << endl;
+    cout << "Reversed Queue ::: ";
+    Reverse(Q);
+    temp = Q;
+    while (!temp.empty())
+    {
+        cout << temp.front() << " ";
+        temp.pop();
+    }
+    cout << endl;
+    return EXIT_SUCCESS;
+}
