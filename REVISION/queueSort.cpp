ff6adbc41f7f9ec75453ca1f5d3e2fb48b10b838
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-23 22:29:46 +0530

    sorting of the queue

--- REVISION/queueSort.cpp
+++ REVISION/queueSort.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
 #include <cstdlib>
 #include <queue>
+#include <climits> // For INT_MAX
 using namespace std;
+
 int FindMinimumIndex(queue<int> q, int sortedIndex)
 {
     int minimumIndex = -1;
-    int minimumvalue = INT_MAX;
-    for (int i = 0; i < q.size(); i++)
+    int minimumValue = INT_MAX;
+    int size = q.size(); // Store the initial size
+
+    for (int i = 0; i < size; i++)
     {
         int current = q.front();
-        if (current < minimumvalue and i <= sortedIndex)
+        q.pop();
+
+        // We only consider the first (sortedIndex + 1) elements
+        if (current < minimumValue && i <= sortedIndex)
         {
             minimumIndex = i;
-            minimumvalue = current;
+            minimumValue = current;
         }
+
         q.push(current);
     }
+
     return minimumIndex;
 }
+
 void insertAtRear(queue<int> &q, int minimumIndex)
 {
     int minimumValue;
     int size = q.size();
-    for (int i = 0; i <= minimumIndex; i++)
+
+    for (int i = 0; i < size; i++)
     {
         int current = q.front();
         q.pop();
+
         if (i != minimumIndex)
         {
             q.push(current);
@@ -35,39 +47,50 @@ void insertAtRear(queue<int> &q, int minimumIndex)
             minimumValue = current;
         }
     }
+
     q.push(minimumValue);
 }
+
 void sortQueue(queue<int> &q)
 {
-    for (int i = 0; i < q.size(); i++)
+    int size = q.size();
+    for (int i = 0; i < size; i++)
     {
-        int minimumIndex = FindMinimumIndex(q, q.size() - i - 1);
+        int minimumIndex = FindMinimumIndex(q, size - i - 1);
         insertAtRear(q, minimumIndex);
     }
 }
+
 void display(queue<int> q)
 {
-    while (not q.empty())
+    while (!q.empty())
     {
         cout << q.front() << " ";
         q.pop();
     }
     cout << endl;
 }
+
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif // !ONLINE_JUDGE
     queue<int> q;
     q.push(3);
     q.push(1);
     q.push(4);
     q.push(-2);
     q.push(0);
+
+    cout << "Original queue: ";
     display(q);
+
     sortQueue(q);
+
+    cout << "Sorted queue: ";
     display(q);
+
     return EXIT_SUCCESS;
 }
