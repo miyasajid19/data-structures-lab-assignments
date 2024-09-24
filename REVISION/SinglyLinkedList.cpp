273e5fd6077cfc05deb3ecb19f83e5f398e33b19
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-24 08:36:25 +0530

    insertion complete

--- REVISION/SinglyLinkedList.cpp
+++ REVISION/SinglyLinkedList.cpp
@@ -1,7 +1,76 @@
 #include <iostream>
-#include<cstdlib>
+#include <cstdlib>
 using namespace std;
+class Node
+{
+public:
+    int Value;
+    Node *Next;
+    Node(int value)
+    {
+        this->Value = value;
+        this->Next = nullptr;
+    }
+};
+class LinkedLists
+{
+    Node *head;
 
+public:
+    LinkedLists()
+    {
+        this->head = nullptr;
+    }
+    void InsertAtHead(int value)
+    {
+        Node *new_node = new Node(value);
+        if (head == nullptr)
+        {
+            head = new_node;
+            return;
+        }
+        new_node->Next = head;
+        head = new_node;
+    }
+    void InsertAtTail(int value)
+    {
+        Node *new_node = new Node(value);
+        if (head == nullptr)
+        {
+            head = new_node;
+            return;
+        }
+        Node *temp = head;
+        while (temp->Next != nullptr)
+        {
+            temp = temp->Next;
+        }
+        temp->Next = new_node;
+    }
+    void Insert(int value, int index)
+    {
+        if (index == 0)
+        {
+            InsertAtHead(value);
+            return;
+        }
+        Node *new_node = new Node(value);
+        int counter = 0;
+        Node *temp = head;
+        while (temp != nullptr and counter <= index)
+        {
+            temp = temp->Next;
+        }
+        if (temp == nullptr)
+        {
+            cout << "invlaid indexing" << endl;
+            delete new_node;
+            return;
+        }
+        new_node->Next = temp->Next;
+        temp->Next = new_node;
+    }
+};
 int main()
 {
 #ifndef ONLINE_JUDGE
