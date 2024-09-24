53ea7f94c31f52f2d00590d13d4446a9ae76bff9
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-24 08:16:34 +0530

    multiplication of sparse matrix

--- REVISION/SparseMatrix.cpp
+++ REVISION/SparseMatrix.cpp
@@ -185,14 +185,14 @@ public:
             }
         }
     }
-     void Transpose()
+    void Transpose()
     {
         transposedMatrix = new int *[this->nonZeros + 1];
         for (int i = 0; i <= this->nonZeros; i++)
         {
             transposedMatrix[i] = new int[3];
         }
-        transposedMatrix[0][0] = this->columns; 
+        transposedMatrix[0][0] = this->columns;
         transposedMatrix[0][1] = this->rows;
         transposedMatrix[0][2] = this->nonZeros;
 
@@ -221,6 +221,91 @@ public:
             cout << transposedMatrix[i][0] << "\t" << transposedMatrix[i][1] << "\t" << transposedMatrix[i][2] << endl;
         }
     }
+    void Mul(SparseMatrix other)
+    {
+        // if (this->columns != A.rows)
+        // {
+        //     cout << "Matrix dimension is not valid" << endl;
+        //     exit(1);
+        // }
+        // SparseMatrix result(this->rows, A.columns);
+        // int count=0;
+        // for (int i = 1; i <= nonZeros; i++)
+        // {
+        //     for (int j = 1; j <= A.nonZeros; j++)
+        //     {
+        //         if (this->sparseMatrix[i][1] == A.sparseMatrix[j][0])
+        //         {
+        //             count++;
+        //             result.sparseMatrix[this->sparseMatrix[i][0]][A.sparseMatrix[j][1]] += this->sparseMatrix[i][2] * A.sparseMatrix[j][2];
+        //         }
+        //     }
+        // }
+        if (this->columns != other.rows)
+        {
+            cout << "Matrices cannot be multiplied due to dimension mismatch." << endl;
+            return;
+        }
+
+        int **result = new int *[this->rows];
+        for (int i = 0; i < this->rows; i++)
+        {
+            result[i] = new int[other.columns]();
+        }
+
+        for (int i = 1; i <= this->nonZeros; i++)
+        {
+            for (int j = 1; j <= other.nonZeros; j++)
+            {
+                if (this->sparseMatrix[i][1] == other.sparseMatrix[j][0])
+                {
+                    result[this->sparseMatrix[i][0]][other.sparseMatrix[j][1]] +=
+                        this->sparseMatrix[i][2] * other.sparseMatrix[j][2];
+                }
+            }
+        }
+
+        int resultNonZeros = 0;
+        for (int i = 0; i < this->rows; i++)
+        {
+            for (int j = 0; j < other.columns; j++)
+            {
+                if (result[i][j] != 0)
+                    resultNonZeros++;
+            }
+        }
+
+        int **sparseResult = new int *[resultNonZeros + 1];
+        for (int i = 0; i <= resultNonZeros; i++)
+        {
+            sparseResult[i] = new int[3];
+        }
+        sparseResult[0][0] = this->rows;
+        sparseResult[0][1] = other.columns;
+        sparseResult[0][2] = resultNonZeros;
+
+        int k = 1;
+        for (int i = 0; i < this->rows; i++)
+        {
+            for (int j = 0; j < other.columns; j++)
+            {
+                if (result[i][j] != 0)
+                {
+                    sparseResult[k][0] = i;
+                    sparseResult[k][1] = j;
+                    sparseResult[k][2] = result[i][j];
+                    k++;
+                }
+            }
+        }
+
+        cout << "Resultant Sparse Matrix after Multiplication:" << endl;
+        cout << "Row\tCol\tValue" << endl;
+        for (int i = 0; i <= resultNonZeros; i++)
+        {
+            cout << sparseResult[i][0] << "\t" << sparseResult[i][1] << "\t" << sparseResult[i][2] << endl;
+        }
+    }
 };
 int main()
 {
@@ -234,12 +319,25 @@ int main()
     mat.DisplaySparse();
     mat.Transpose();
     mat.displayTransposedSparse();
-    // SparseMatrix mat1(3, 3);
-    // mat1.setMatrix();
-    // mat1.SparseIt();
-    // mat1.DisplaySparse();
-    // SparseMatrix result = mat1 + mat1 + mat + mat;
-    // result.DisplaySparse();
-    // result.Transpose();
+    SparseMatrix mat1(3, 3);
+    mat1.setMatrix();
+    mat1.SparseIt();
+    mat1.DisplaySparse();
+    SparseMatrix result = mat1 + mat1 + mat + mat;
+    result.DisplaySparse();
+    result.Transpose();
+
+
+
+    cout<<endl<<endl;
+    SparseMatrix A(2,2);
+    A.setMatrix();
+    A.SparseIt();
+    A.Transpose();
+    SparseMatrix B(2,2);
+    B.setMatrix();
+    B.SparseIt();
+    B.Transpose();
+    A.Mul(B);
     return EXIT_SUCCESS;
 }
