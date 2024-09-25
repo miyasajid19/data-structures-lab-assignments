3336aace554d5ede8407e3770febb48a917d68a7
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-25 09:51:17 +0530

    find maximum element in the array using recurrsion

--- REVISION/MaxInArr.cpp
+++ REVISION/MaxInArr.cpp
@@ -10,6 +10,12 @@ public:
             return arr[index];
         return max(arr[index], MaxInArr(arr, index + 1, size));
     }
+    int MinInArr(int arr[], int index, int size)
+    {
+        if (index == size - 1)
+            return arr[index];
+        return min(arr[index], MaxInArr(arr, index + 1, size));
+    }
 };
 int main()
 {
@@ -17,8 +23,9 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-int arr[]={1,4,12,8,39,432,407,2062,2061};
-Solution soln;
-cout<<soln.MaxInArr(arr,0,sizeof(arr)/sizeof(arr[0]));
+    int arr[] = {1, 4, 12, 8, 39, 432, 407, 2062, 2061};
+    Solution soln;
+    cout << soln.MaxInArr(arr, 0, sizeof(arr) / sizeof(arr[0])) << endl;
+    cout << soln.MinInArr(arr, 0, sizeof(arr) / sizeof(arr[0])) << endl;
     return EXIT_SUCCESS;
 }
