ad6f13cdcf80068ed542aa83411bd5461e9f191f
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-25 09:50:17 +0530

    find maximum element in the array

---
+++ REVISION/MaxInArr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+class Solution
+{
+public:
+    int MaxInArr(int arr[], int index, int size)
+    {
+        if (index == size - 1)
+            return arr[index];
+        return max(arr[index], MaxInArr(arr, index + 1, size));
+    }
+};
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+int arr[]={1,4,12,8,39,432,407,2062,2061};
+Solution soln;
+cout<<soln.MaxInArr(arr,0,sizeof(arr)/sizeof(arr[0]));
+    return EXIT_SUCCESS;
+}
