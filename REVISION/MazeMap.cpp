595e9774223c1c5cbd93bfa02980007517fed01a
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-25 10:00:15 +0530

    mazeMap recurrsion

--- REVISION/MazeMap.cpp
+++ REVISION/MazeMap.cpp
@@ -4,14 +4,14 @@ using namespace std;
 class Solution
 {
 public:
-    int MazeMapping(int row, int column, int rows, int columns)
-    {
-        if (row == rows - 1 and column == columns - 1)
-            return 1;
-        if (row > rows or columns > columns)
-            return 0;
-        return MazeMapping(row + 1, column, rows, columns) + MazeMapping(row, column + 1, rows, columns);
-    }
+    int mazeMap(int row, int column, int rows, int columns)
+{
+    if (row == rows - 1 and column == columns - 1)
+        return 1;
+    if (row >= rows or column > columns)
+        return 0;
+    return mazeMap(row + 1, column, rows, columns) + mazeMap(row, column + 1, rows, columns);
+}
 };
 int main()
 {
@@ -20,6 +20,6 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
     Solution soln;
-    cout << soln.MazeMapping(0, 0, 10, 4);
+    cout << soln.mazeMap(0, 0, 432, 407);
     return EXIT_SUCCESS;
 }
