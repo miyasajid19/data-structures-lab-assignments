144147a87f6514d5ef43d4d3e2d3704e8acb178b
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-21 23:29:44 +0530

    add solution for the first part of first question

---
+++ week9/Question_1_i.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+class Heap
+{
+public:
+    void heapify(int arr[], int size, int index)
+    {
+        int largest = index;
+        int leftIndex = 2 * index + 1;
+        int rightIndex = 2 * index + 2;
+
+        if (leftIndex < size and arr[largest] < arr[leftIndex])
+            largest = leftIndex;
+        if (rightIndex < size and arr[largest] < arr[rightIndex])
+            largest = rightIndex;
+        if (largest != index)
+        {
+            swap(arr[largest], arr[index]);
+            heapify(arr, size, largest);
+        }
+    }
+    void sortArr(int arr[], int size)
+    {
+        for (int i = size / 2 - 1; i >= 0; i--)
+        {
+            heapify(arr, size, i);
+        }
+        for (int i = size - 1; i > 0; i--)
+        {
+            swap(arr[i], arr[0]);
+            heapify(arr, i, 0);
+        }
+    }
+};
+int main()
+{
+    Heap heap;
+    int arr[] = {7, 8, 9, 0, 5, 4, 3, 2, 1};
+    int size = sizeof(arr) / sizeof(int);
+    cout << "unsorted array :: ";
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << '\t';
+    cout << endl;
+    heap.sortArr(arr, size);
+    cout << "sorted array :: ";
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << '\t';
+    cout << endl;
+    return EXIT_SUCCESS;
+}
