915f84f02e1f0f36ee0394db01359e75b5e7e7a2
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-21 20:07:48 +0530

    temp

--- week8/Question_3.cpp
+++ week8/Question_3.cpp
@@ -2,7 +2,8 @@
 #include <queue>
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     int value;
     Node *left;
@@ -10,11 +11,14 @@ public:
     Node(int value) : value(value), left(nullptr), right(nullptr) {}
 };
 
-class BST {
+class BST
+{
     Node *root;
 
-    void levelorder(Node *temp) {
-        if (temp == nullptr) {
+    void levelorder(Node *temp)
+    {
+        if (temp == nullptr)
+        {
             cout << "Tree is empty." << endl;
             return;
         }
@@ -25,18 +29,23 @@ class BST {
         Queue.push(temp);
         Queue.push(nullptr); // Marker for level separation
 
-        while (!Queue.empty()) {
+        while (!Queue.empty())
+        {
             Node *TopNode = Queue.front();
             Queue.pop();
 
-            if (TopNode == nullptr) {
+            if (TopNode == nullptr)
+            {
                 cout << endl;
                 level++;
-                if (!Queue.empty()) {
+                if (!Queue.empty())
+                {
                     cout << "Level " << level << " :: ";
                     Queue.push(nullptr); // Add marker for next level
                 }
-            } else {
+            }
+            else
+            {
                 cout << TopNode->value << '\t';
                 if (TopNode->left)
                     Queue.push(TopNode->left);
@@ -46,7 +55,8 @@ class BST {
         }
     }
 
-    Node *buildTree(Node *temp, int value) {
+    Node *buildTree(Node *temp, int value)
+    {
         if (temp == nullptr)
             return new Node(value);
 
@@ -57,36 +67,142 @@ class BST {
 
         return temp;
     }
+    Node *FindMax(Node *temp)
+    {
+        while (temp->right != nullptr)
+        {
+            temp = temp->right;
+        }
+        return temp;
+    }
+    Node *FindMin(Node *temp)
+    {
+        while (temp->left != nullptr)
+        {
+            temp = temp->left;
+        }
+        return temp;
+    }
+    void inorder(Node *temp)
+    {
+        if (temp == nullptr)
+            return;
+        inorder(temp->left);
+        cout << temp->value << "\t";
+        inorder(temp->right);
+    }
+    Node *deleteNode(Node *temp, int value, bool replacementfromLefttoright = false)
+    {
+        if (temp = nullptr)
+        {
+            return temp;
+        }
+        if (value > temp->value)
+        {
+            temp->right = deleteNode(temp->right, value, replacementfromLefttoright);
+        }
+        else if (value < temp->value)
+        {
+            temp->left = deleteNode(temp->left, value, replacementfromLefttoright);
+        }
+        else
+        {
+            // we found the node that we have to delete
+            if (temp->left == nullptr and temp->right == nullptr)
+            {
+                delete temp;
+                return nullptr;
+            }
+            // if it has only left child
+            if (temp->left != nullptr and temp->right == nullptr)
+            {
+                Node *leftchild = temp->left;
+                delete temp;
+                return leftchild;
+            }
+            if (temp->left != nullptr and temp->right == nullptr)
+            {
+                Node *leftchild = temp->left;
+                delete temp;
+                return leftchild;
+            }
+            // if has only right child
+            if (temp->right != nullptr and temp->left == nullptr)
+            {
+                Node *rightchild = temp->right;
+                delete temp;
+                return rightchild;
+            }
+            // if have both left and right child
+            if (replacementfromLefttoright)
+            {
+                Node *maxNode = FindMax(temp->left);
+                temp->value = maxNode->value;
+                temp->left = deleteNode(temp->left, maxNode->value, replacementfromLefttoright);
+            }
+            else
+            {
+                Node *minNode = FindMin(temp->right);
+                temp->value = minNode->value;
+                temp->left = deleteNode(temp->right, minNode->value, replacementfromLefttoright);
+            }
+        }
+        return temp;
+    }
 
 public:
     BST() : root(nullptr) {}
 
-    void BuildTree() {
+    void BuildTree()
+    {
         int value;
         cout << "Enter elements to build the BST (enter -1 to stop): " << endl;
         cin >> value;
-        while (value != -1) {
+        while (value != -1)
+        {
             root = buildTree(root, value);
             cin >> value;
         }
     }
 
-    void levelOrderTraversal() {
+    void levelOrderTraversal()
+    {
         cout << "Level Order Traversal: " << endl;
         levelorder(root);
         cout << endl;
     }
+    void Inorder()
+    {
+        cout << "Inorder Traversal ::: ";
+        inorder(root);
+        cout << endl;
+    }
+    void insert(int value)
+    {
+        root = buildTree(root, value);
+    }
+    void DeleteNode(int value, bool key = false)
+    {
+        root = deleteNode(root, value, key);
+    }
 };
 
-int main() {
+int main()
+{
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
 
     BST bst;
-    bst.BuildTree();
+    bst.BuildTree(); // from this we cannot add -1
     bst.levelOrderTraversal();
-
+    bst.Inorder();
+    bst.insert(-1); // we can insert -1  by this
+    bst.levelOrderTraversal();
+    bst.Inorder();
+    bst.DeleteNode(9);
+    bst.levelOrderTraversal();
+    bst.Inorder();
     return EXIT_SUCCESS;
 }
