b23d780c00be821a7995e526b59112dbcce1fe18
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-21 20:46:08 +0530

    add solution for the fifth question

---
+++ week8/Question_5.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+    Node(int value) : value(value), left(nullptr), right(nullptr) {}
+};
+
+class BST
+{
+    Node *root;
+
+    void levelorder(Node *temp)
+    {
+        if (temp == nullptr)
+        {
+            cout << "Tree is empty." << endl;
+            return;
+        }
+
+        queue<Node *> Queue;
+        int level = 0;
+        cout << "Level " << level << " :: ";
+        Queue.push(temp);
+        Queue.push(nullptr); // Marker for level separation
+
+        while (!Queue.empty())
+        {
+            Node *TopNode = Queue.front();
+            Queue.pop();
+
+            if (TopNode == nullptr)
+            {
+                cout << endl;
+                level++;
+                if (!Queue.empty())
+                {
+                    cout << "Level " << level << " :: ";
+                    Queue.push(nullptr); // Add marker for next level
+                }
+            }
+            else
+            {
+                cout << TopNode->value << '\t';
+                if (TopNode->left)
+                    Queue.push(TopNode->left);
+                if (TopNode->right)
+                    Queue.push(TopNode->right);
+            }
+        }
+    }
+
+    Node *buildTree(Node *temp, int value)
+    {
+        if (temp == nullptr)
+            return new Node(value);
+
+        if (value > temp->value)
+            temp->right = buildTree(temp->right, value);
+        else if (value < temp->value)
+            temp->left = buildTree(temp->left, value);
+
+        return temp;
+    }
+
+    void inorder(Node *temp)
+    {
+        stack<Node *> stk;
+        Node *currentNode = temp;
+        while (currentNode != nullptr or not stk.empty())
+        {
+            while (currentNode != nullptr)
+            {
+                stk.push(currentNode);
+                currentNode = currentNode->left;
+            }
+            currentNode = stk.top();
+            stk.pop();
+            cout << currentNode->value << "\t";
+            currentNode = currentNode->right;
+        }
+    }
+
+public:
+    BST() : root(nullptr) {}
+
+    void BuildTree()
+    {
+        int value;
+        cout << "Enter elements to build the BST (enter -1 to stop): " << endl;
+        cin >> value;
+        while (value != -1)
+        {
+            root = buildTree(root, value);
+            cin >> value;
+        }
+    }
+
+    void levelOrderTraversal()
+    {
+        cout << "Level Order Traversal: " << endl;
+        levelorder(root);
+        cout << endl;
+    }
+
+    void Inorder()
+    {
+        cout << "Inorder Traversal ::: ";
+        inorder(root);
+        cout << endl;
+    }
+};
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    BST bst;
+    bst.BuildTree(); // from this we cannot add -1
+    bst.levelOrderTraversal();
+    bst.Inorder();
+    return EXIT_SUCCESS;
+}
